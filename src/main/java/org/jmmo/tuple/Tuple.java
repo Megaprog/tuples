7356b4c01857dcb5ea7a8637bbe1d510beef6173
Tomas Shestakov <shestakovte@gmail.com>
2018-01-08 01:04:13 +0300

    Tuple skeleton interface have been created

--- src/main/java/org/jmmo/tuple/Tuple.java
+++ src/main/java/org/jmmo/tuple/Tuple.java
@@ -1,6 +1,45 @@
 package org.jmmo.tuple;
 
 import java.io.Serializable;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
-public interface Tuple extends Serializable, Iterable<Object>, Comparable<Tuple> {
+interface Tuple<T> extends Serializable, Iterable<T>, Comparable<Tuple> {
+
+    int getSize();
+
+    <E extends T> E get(int i);
+
+    Object[] toArray();
+
+    @Override default Iterator<T> iterator() {
+        return new TupleIterator<>(this);
+    }
+
+    @Override default Spliterator<T> spliterator() {
+        return Spliterators.spliterator(iterator(), getSize(),
+                Spliterator.SIZED | Spliterator.IMMUTABLE | Spliterator.ORDERED);
+    }
+
+    default Stream<T> stream() {
+        return StreamSupport.stream(spliterator(), false);
+    }
+
+    class TupleIterator<T> implements Iterator<T> {
+        private final Tuple<T> tuple;
+        private int index;
+
+        TupleIterator(Tuple<T> tuple) {this.tuple = tuple;}
+
+        @Override public boolean hasNext() {
+            return index < tuple.getSize();
+        }
+
+        @Override public T next() {
+            return tuple.get(index++);
+        }
+    }
 }
