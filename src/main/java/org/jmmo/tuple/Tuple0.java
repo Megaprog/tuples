e5ab876607e56292799b39055d19ab5a0b5cdec3
Tomas Shestakov <shestakovte@gmail.com>
2018-01-09 00:18:37 +0300

    Tuple1 has been created

--- src/main/java/org/jmmo/tuple/Tuple0.java
+++ src/main/java/org/jmmo/tuple/Tuple0.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 public class Tuple0 implements Tuple<Void>, Comparable<Tuple0> {
     private static final long serialVersionUID = -5035901162805121584L;
 
-    private static final Tuple0 INSTANCE = new Tuple0();;
+    private static final Tuple0 INSTANCE = new Tuple0();
 
     protected Tuple0() {
     }
@@ -19,7 +19,7 @@ public class Tuple0 implements Tuple<Void>, Comparable<Tuple0> {
     }
 
     @Override public <E extends Void> E get(int i) {
-        throw new IndexOutOfBoundsException("Tuple0 has no elements");
+        throw new IndexOutOfBoundsException("Tuple0 has no elements but " + i + " element requested");
     }
 
     @Override public int compareTo(Tuple0 o) {
@@ -28,13 +28,13 @@ public class Tuple0 implements Tuple<Void>, Comparable<Tuple0> {
 
     @Override public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Tuple1<?> other = (Tuple1<?>) o;
+        if (!(o instanceof Tuple0)) return false;
+        Tuple0 other = (Tuple0) o;
         return other.getSize() == 0;
     }
 
     @Override public int hashCode() {
-        return 0;
+        return 1;
     }
 
     @Override public String toString() {
---
+++ src/main/java/org/jmmo/tuple/Tuple1.java
@@ -0,0 +1,80 @@
+package org.jmmo.tuple;
+
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class Tuple1<T> implements Tuple<T>, Comparable<Tuple1<T>> {
+    private static final long serialVersionUID = -8854900772811744378L;
+
+    private final T value0;
+
+    protected Tuple1(T value0) {this.value0 = value0;}
+
+    public <E> Tuple1<E> of(E value0) {
+        return new Tuple1<>(value0);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <A, E extends A> Tuple1<E> fromArray(A[] values) {
+        return new Tuple1<>((E) values[0]);
+    }
+
+    public <E> Tuple1<E> fromIterator(Iterator<E> iterator) {
+        return new Tuple1<>(iterator.next());
+    }
+
+    public void unfold(Consumer<T> valueConsumer) {
+        valueConsumer.accept(getValue0());
+    }
+
+    public <R> R unfoldResult(Function<T, R> valueFunction) {
+        return valueFunction.apply(getValue0());
+    }
+
+    public T getValue0() {
+        return value0;
+    }
+
+    @Override public int getSize() {
+        return 1;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override public <E extends T> E get(int i) {
+        switch (i) {
+            case 0: return (E) getValue0();
+        }
+
+        throw new IndexOutOfBoundsException("Tuple1 has one element but " + i + " element requested");
+    }
+
+    @Override public Object[] toArray() {
+        return new Object[] { getValue0() };
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override public int compareTo(Tuple1<T> o) {
+        return Comparator.nullsFirst(Comparator.<Comparable>naturalOrder())
+                .compare((Comparable) this.getValue0(), (Comparable) o.getValue0());
+    }
+
+    @Override public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Tuple1)) return false;
+        Tuple1<?> tuple1 = (Tuple1<?>) o;
+        return Objects.equals(getValue0(), tuple1.getValue0());
+    }
+
+    @Override public int hashCode() {
+        return Objects.hash(getValue0());
+    }
+
+    @Override public String toString() {
+        return "Tuple1{" +
+                "value0=" + getValue0() +
+                '}';
+    }
+}
